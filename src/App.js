f50e5c5fb87af9d4ceced44c25c7e1ca0fad108c
Wynn Drahorad <wynnd5595@gmail.com>
2018-04-14 23:37:51 -0500

    add partial redux implementation, add code to play around with d3, add linting support, add react-redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import * as d3 from 'd3'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className='chart'>
+        </div>
       </div>
-    );
+    )
+  }
+
+  componentDidMount () {
+    this.barGraph()
+  }
+
+  barGraph () {
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const scale = d3.scaleLinear().domain([0, 10]).range([0, 100])
+    const chart = d3.select('.chart')
+    const bars = chart.selectAll('.bar')
+    const barUpdate = bars.data(array)
+    const barEnter = barUpdate.enter().append('div')
+      .attr('class', 'bar')
+      .style('width', d => scale(d) + '%')
+      .style('background-color', d => `rgb(255, ${255 * (scale(d) / 100.0)}, 255)`)
+      .style('text-align', 'right')
+      .text(d => d)
   }
 }
 
-export default App;
+export default App
